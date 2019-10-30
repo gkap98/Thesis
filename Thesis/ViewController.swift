6105ac73177fb997bb645d318770891099b7d65f
Gavin Kaepernick <kaepernick.gavin6@gmail.com>
2019-10-29 19:11:28 -0500

    Initial Commit

---
+++ Thesis/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Thesis
+//
+//  Created by Gavin Kaepernick on 10/29/19.
+//  Copyright © 2019 Gavin Kaepernick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
